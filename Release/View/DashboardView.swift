6727516ea88eea5346afc2dee1a4579dafb6cd2a
Ahmad F. Iksan <ahmad.f.iksan@gmail.com>
2023-07-20 14:59:28 +0700

    add navigation stack to Dashboard view

--- Release/View/DashboardView.swift
+++ Release/View/DashboardView.swift
@@ -9,31 +9,35 @@ import SwiftUI
 
 struct DashboardView: View {
     let emotions: [String] = ["Angry", "Sad", "Furious", "Dissapointed"]
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
     @State var emotionsIndex: Int = 0
     var body: some View {
-        VStack{
-            Text("How are you feeling today?").font(.custom("Poppins-Black", size: 28)).fontWeight(.semibold).frame(width: 250).multilineTextAlignment(.center)
+            VStack{
+                Text("How are you feeling today?").font(.custom("Poppins-Black", size: 28)).fontWeight(.semibold).frame(width: 250).multilineTextAlignment(.center)
 
-            Button{}label: {
-                ZStack{
-                    Circle().frame(width: 200).foregroundColor(Color("Primary"))
-                    Text(emotions[emotionsIndex]).font(.custom("Poppins-Black", size: 28)).fontWeight(.medium).foregroundColor(.white)
+                Button{}label: {
+                    ZStack{
+                        Circle().frame(width: 200).foregroundColor(Color("Primary"))
+                        Text(emotions[emotionsIndex]).font(.custom("Poppins-Black", size: 28)).fontWeight(.medium).foregroundColor(.white)
+                    }
                 }
             }
-        }
-        .onAppear{
-            // Activate timet to run generateNum function every second
-            Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
-                withAnimation{
-                    generateNum()
-                }
+            .onAppear{
 
-                })
-        }
-        .onDisappear{
-            // Turn of the timer when view disappear
-            Timer.cancelPreviousPerformRequests(withTarget: generateNum())
-        }
+                isOnboarding = true
+                // Activate timet to run generateNum function every second
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
+                    withAnimation{
+                        generateNum()
+                    }
+
+                    })
+            }
+            .onDisappear{
+                // Turn of the timer when view disappear
+                Timer.cancelPreviousPerformRequests(withTarget: generateNum())
+            }.navigationBarBackButtonHidden()
+
     }
 
     // Randomize the text inside the circle
