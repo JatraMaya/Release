ead701e1127ef86e682190d26dcdd579b8db15de
Ahmad F. Iksan <ahmad.f.iksan@gmail.com>
2023-07-25 14:56:30 +0700

    Generate new project.yml

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Ahmad Fadly Iksan on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
