0c6a57846a2a3c26e513211a91060924fa22e09b
Ahmad F. Iksan <ahmad.f.iksan@gmail.com>
2023-07-25 12:24:09 +0700

    Add DateDisplayView

---
+++ Release/ViewModel/DateDisplayView.swift
@@ -0,0 +1,25 @@
+//
+//  DateDisplayView.swift
+//  Release!
+//
+//  Created by Ahmad Fadly Iksan on 25/07/23.
+//
+
+import SwiftUI
+
+struct DateDisplayView: View {
+    var date: Date = Date()
+    var body: some View {
+        HStack{
+                Text("\(date.formatted(.dateTime.weekday(.abbreviated))),")
+                Text(date.formatted(.dateTime.day().month().year().locale(Locale(identifier: "id_ID"))))
+        }
+        .frame(width: 200, alignment: .trailing)
+    }
+}
+
+struct DateDisplayView_Previews: PreviewProvider {
+    static var previews: some View {
+        DateDisplayView()
+    }
+}
---
+++ Release/ViewModel/JournalContentViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  JournalContentViewModel.swift
+//  Release!
+//
+//  Created by Ahmad Fadly Iksan on 25/07/23.
+//
+
+import SwiftUI
+
+struct JournalContentViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JournalContentViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        JournalContentViewModel()
+    }
+}
