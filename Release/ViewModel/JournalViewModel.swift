a380d900d0eafce4aa7618d1517a43447bf9feed
Ahmad F. Iksan <ahmad.f.iksan@gmail.com>
2023-07-25 09:12:49 +0700

    Add JournalViewModel file

--- Release/ViewModel/JournalViewModel.swift
+++ Release/ViewModel/JournalViewModel.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct JournalViewModel: View {
-    var moodBefore: String = "a"
-    var moodAfter: String = "a"
+    var moodBefore: String = "angry"
+    var moodAfter: String = "furious"
     var content: String = "some content"
     var index: Int = 1
    var date: Date = Date()
@@ -17,37 +17,55 @@ struct JournalViewModel: View {
         VStack{
             HStack{
                 Spacer()
-                Image(moodAfter).resizable().frame(width: 30, height: 30)
+                ZStack{
+                    Circle()
+                        .fill(Color("Primary"))
+                        .frame(width: 32, height: 32)
+                    Image(moodAfter).resizable().frame(width: 30, height: 30)
+                }
+
                 Image(systemName: "arrow.right").foregroundColor(Color("Primary"))
-                Image(moodBefore).resizable().frame(width: 30, height: 30)
+                ZStack{
+                    Circle()
+                        .fill(Color("Primary"))
+                        .frame(width: 32, height: 32)
+                    Image(moodBefore).resizable().frame(width: 30, height: 30)
+                }
                 Spacer()
                 Text(date.formatted(.dateTime.weekday(.wide).day().month().year()))
                 Spacer()
             }
             HStack{
-//                Spacer()
+                Spacer()
+                Spacer()
                 Divider()
                     .frame(minWidth: 3)
                     .background(Color("Primary")).opacity(0.5)
-//                Text("\(index)")
+                Spacer()
+                Spacer()
                 Text(content)
-
-                    .padding(.leading, 50)
-                    .frame(width: 300, height: 80, alignment: .leading)
-
+                    .font(.custom("Poppins", size: 12))
+                    .padding([.leading, .trailing], 10)
+                    .frame(width: 280, height: 90, alignment: .leading)
                     .foregroundColor(index % 2 == 0 ? .white : .black)
-                    .background(index % 2 == 0 ? Color("Primary") : .white).cornerRadius(radius: 20, corners: .allCorners)
-                    .cornerRadius(radius: 10, corners: .allCorners)
-                    .border(index % 2 == 0 ? .white : Color("Primary"))
+                    .background(index % 2 == 0 ? Color("Primary") : .white)
+                    .cornerRadius(radius: 25, corners: .allCorners)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 25)
+                            .stroke(index % 2 == 0 ? .blue : Color("Primary"), lineWidth: 2).opacity(index % 2 == 0 ? 0 : 1)
+                    )
 
-            }.frame(width: 350, height: 114, alignment: .center)
-                .padding(.bottom, 10)
+//                    .border(index % 2 == 0 ? .white : Color("Primary"))
+
+            }.frame(width: 300, height: 100, alignment: .center)
+//                .padding(.bottom, 10)
+//                .background(.black)
         }
     }
 }
 
 struct JournalViewModel_Previews: PreviewProvider {
     static var previews: some View {
-        JournalViewModel(moodBefore: "a", date: Date())
+        JournalViewModel()
     }
 }
