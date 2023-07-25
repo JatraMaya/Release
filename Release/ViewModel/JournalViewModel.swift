94b5a5a13d7ca97690700d58d7806688717f1586
Ahmad F. Iksan <ahmad.f.iksan@gmail.com>
2023-07-25 12:28:10 +0700

    Separate some View into it's own ViewModel files

--- Release/ViewModel/JournalViewModel.swift
+++ Release/ViewModel/JournalViewModel.swift
@@ -11,44 +11,12 @@ struct JournalViewModel: View {
     var moodBefore: String = "😠"
     var moodAfter: String = "🤬"
     var content: String = "some content"
-    var index: Int = 1
+    var index: Int = 0
     var date: Date = Date()
     var body: some View {
         VStack{
-            HStack{
-                Spacer()
-                Text(moodAfter)
-                Image(systemName: "arrow.right").foregroundColor(Color("Primary"))
-                Text(moodBefore)
-                Spacer()
-                HStack{
-                    Group{
-                        Text("\(date.formatted(.dateTime.weekday(.abbreviated))),")
-                        Text(date.formatted(.dateTime.day().month().year().locale(Locale(identifier: "id_ID"))))
-                    }
-                }
-                Spacer()
-            }
-            HStack{
-                Spacer()
-                Spacer()
-                Divider()
-                    .frame(minWidth: 3)
-                    .background(Color("Primary")).opacity(0.5)
-                Spacer()
-                Spacer()
-                Text(content)
-                    .font(.custom("Poppins", size: 12))
-                    .padding([.leading, .trailing], 10)
-                    .frame(width: 280, height: 90, alignment: .leading)
-                    .foregroundColor(index % 2 == 0 ? .white : .black)
-                    .background(index % 2 == 0 ? Color("Primary") : .white)
-                    .cornerRadius(radius: 25, corners: .allCorners)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 25)
-                            .stroke(index % 2 == 0 ? .blue : Color("Primary"), lineWidth: 2).opacity(index % 2 == 0 ? 0 : 1)
-                    )
-            }.frame(width: 300, height: 100, alignment: .center)
+            MoodBeforeAfterViewModel(moodBefore: moodBefore, moodAfter: moodAfter, date: date)
+            JournalContentViewModel(content: content, index: index)
         }
     }
 }
