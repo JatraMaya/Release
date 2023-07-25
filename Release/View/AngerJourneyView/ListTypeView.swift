8b0aa43a0537f946d7530b1025474b1089e1cd5d
Ahmad F. Iksan <ahmad.f.iksan@gmail.com>
2023-07-25 09:14:35 +0700

    Adding ListTypeView for AngerJourney

--- Release/View/AngerJourneyView/ListTypeView.swift
+++ Release/View/AngerJourneyView/ListTypeView.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct ListTypeView: View {
-    var journal: [JournalModel] = []
-//    var journal: [JournalModel] = [JournalModel(moodBefore: "a", moodAfter: "b", content: "hahahahaha, masalah lorem ipsum dolret sit amet, somedipa, semotthing, is in the way and I don't know what I'm trying to ", date: Date()), JournalModel(moodBefore: "a", moodAfter: "b", content: "hahahahahax", date: Date()), JournalModel(moodBefore: "a", moodAfter: "b", content: "hahahahaha", date: Date()),
-//        JournalModel(moodBefore: "a", moodAfter: "b", content: "hahahahaha, masalah lorem ipsum dolret sit amet, somedipais in the way and I don't know what I'm trying to ", date: Date()),
-//            JournalModel(moodBefore: "a", moodAfter: "b", content: "hahahahaha, masalah lorem ipsum dolret sit amet, somedipa, semotthing, is in the way and I don't know what I'm trying to ", date: Date())]
+//    var journal: [JournalModel] = []
+    var journal: [JournalModel] = [JournalModel(moodBefore: "angry", moodAfter: "furious", content: "hahahahaha, masalah lorem ipsum dolret sit amet, somedipa, semotthing, is in the way and I don't know what I'm trying to ", date: Date()), JournalModel(moodBefore: "upset", moodAfter: "mad", content: "hahahahahax", date: Date()), JournalModel(moodBefore: "a", moodAfter: "b", content: "hahahahaha", date: Date()),
+        JournalModel(moodBefore: "furious", moodAfter: "mad", content: "hahahahaha, masalah lorem ipsum dolret sit amet, somedipais in the way and I don't know what I'm trying to ", date: Date()),
+            JournalModel(moodBefore: "a", moodAfter: "b", content: "hahahahaha, masalah lorem ipsum dolret sit amet, somedipa, semotthing, is in the way and I don't know what I'm trying to ", date: Date())]
     var body: some View {
         VStack{
             ScrollView{
