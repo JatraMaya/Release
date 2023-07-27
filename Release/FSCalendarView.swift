0d12eac6ff4ad24b597b2bb92f1f62acebe48f8d
Ahmad F. Iksan <ahmad.f.iksan@gmail.com>
2023-07-27 12:15:53 +0700

    Fix CalendarView

--- Release/FSCalendarView.swift
+++ Release/FSCalendarView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 import FSCalendar
 
 struct CalendarView: UIViewRepresentable {
-    @State var reservedDates: [JournalModel] = []
+    @State var reservedDates: [String] = []
+    let calendarMode: CalendarMode
 
-    init(_ reservedDates: [JournalModel]) {
+    init(_ reservedDates: [String], mode calendarMode: CalendarMode) {
         self.reservedDates = reservedDates
+        self.calendarMode = calendarMode
     }
 
-    func makeCoordinator() -> Coordinator{
+    func makeCoordinator() -> Coordinator {
         return Coordinator(reservedDates)
     }
 
@@ -26,19 +28,19 @@ struct CalendarView: UIViewRepresentable {
         calendar.dataSource = context.coordinator
 
         // Event Options
-        calendar.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height )
-        calendar.appearance.todayColor = UIColor.systemGreen
-        calendar.today = nil
-        calendar.appearance.weekdayFont = UIFont.systemFont(ofSize: 13, weight: .semibold)
-        calendar.appearance.weekdayTextColor = .black
-        calendar.appearance.headerTitleColor = .black
-        calendar.appearance.headerTitleFont = UIFont.systemFont(ofSize: 17, weight: .semibold)
-        calendar.appearance.titleFont = UIFont.systemFont(ofSize: 20)
-        calendar.appearance.titlePlaceholderColor = UIColor.white
-        calendar.appearance.caseOptions = FSCalendarCaseOptions.weekdayUsesUpperCase
-        calendar.appearance.headerSeparatorColor = nil
-        calendar.pagingEnabled = false
-        calendar.scrollDirection = .horizontal
+                calendar.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height )
+                calendar.appearance.todayColor = UIColor.systemGreen
+                calendar.today = nil
+                calendar.appearance.weekdayFont = UIFont.systemFont(ofSize: 13, weight: .semibold)
+                calendar.appearance.weekdayTextColor = .black
+                calendar.appearance.headerTitleColor = .black
+                calendar.appearance.headerTitleFont = UIFont.systemFont(ofSize: 17, weight: .semibold)
+                calendar.appearance.titleFont = UIFont.systemFont(ofSize: 20)
+                calendar.appearance.titlePlaceholderColor = UIColor.white
+                calendar.appearance.caseOptions = FSCalendarCaseOptions.weekdayUsesUpperCase
+                calendar.appearance.headerSeparatorColor = nil
+                calendar.pagingEnabled = false
+                calendar.scrollDirection = .horizontal
 
         return calendar
     }
@@ -47,19 +49,61 @@ struct CalendarView: UIViewRepresentable {
 
     }
 
-    final class Coordinator: NSObject, FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
-        let reservedDates: [JournalModel]
+    // class Coordinator: NSObject { }
+    class Coordinator: NSObject, FSCalendarDelegate {
+        let reservedDates: [String]
 
-        init(_ dates: [JournalModel]) {
+        init(_ dates: [String]) {
             self.reservedDates = dates
         }
+    }
+}
 
-        func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-            print("selected")
+
+extension CalendarView {
+    enum CalendarMode {
+        case basicCalendarMode
+    }
+}
+
+//extension TestCal.Coordinator: FSCalendarDelegate { }
+
+extension CalendarView.Coordinator: FSCalendarDataSource {
+
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print("Sugeee")
+    }
+}
+
+extension CalendarView.Coordinator: FSCalendarDelegateAppearance {
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, fillDefaultColorFor date: Date) -> UIColor? {
+        var dateFormatter: DateFormatter {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            return dateFormatter
         }
+        let eventDate = dateFormatter.string(from: date)
+
+        return reservedDates.contains(where: { $0 == eventDate }) ? UIColor(named: "Primary") : nil
+    }
 
 
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleDefaultColorFor date: Date) -> UIColor? {
+        var dateFormatter: DateFormatter {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            return dateFormatter
+        }
+        let eventDate = dateFormatter.string(from: date)
 
+        return reservedDates.contains(where: { $0 == eventDate }) ? .white: .black
+    }
 
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, fillSelectionColorFor date: Date) -> UIColor? {
+        return UIColor.white
+    }
+
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleSelectionColorFor date: Date) -> UIColor? {
+        return .black
     }
 }
